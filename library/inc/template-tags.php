e56a1eef69e77860e137971b59c8f4820f364289
Alonso Avila <avila@zaa.mx>
2015-05-11 14:08:06 -0500

    Paginación numerada

--- library/inc/template-tags.php
+++ library/inc/template-tags.php
@@ -37,6 +37,30 @@ function the_posts_navigation() {
 }
 endif;
 
+if ( ! function_exists( 'wpzaamx_page_navi' ) ) :
+// Numeric Page Navi (built into the theme by default)
+function wpzaamx_page_navi() {
+  global $wp_query;
+  $bignum = 999999999;
+  if ( $wp_query->max_num_pages <= 1 )
+    return;
+  echo '<nav class="pagination">';
+  echo paginate_links( array(
+    'base'         => str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
+    'format'       => '',
+    'current'      => max( 1, get_query_var('paged') ),
+    'total'        => $wp_query->max_num_pages,
+    'prev_text'    => '&larr;',
+    'next_text'    => '&rarr;',
+    'type'         => 'list',
+    'end_size'     => 3,
+    'mid_size'     => 3
+  ) );
+  echo '</nav>';
+} /* end page navi */
+endif;
+
+
 if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
  * Display navigation to next/previous post when applicable.
