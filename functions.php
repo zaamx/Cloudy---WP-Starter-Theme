3fa7777fcbf09c080efe405d8d20c7bdaf40017c
Alonso Avila <avila@zaa.mx>
2015-05-11 12:59:36 -0500

    Estructura de tema, registro de funciones

--- functions.php
+++ functions.php
@@ -12,6 +12,14 @@ if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
 }
 
+
+/**
+ * BS menus support
+ */
+require get_template_directory() . '/library/inc/wp_bootstrap_navwalker.php';
+
+
+
 if ( ! function_exists( 'wpzaamx_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -46,11 +54,17 @@ function wpzaamx_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
+
+	// default thumb size
+	set_post_thumbnail_size(125, 125, true);
+
 
 	// This theme uses wp_nav_menu() in one location.
+	add_theme_support( 'menus' );
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'wpzaamx' ),
+		'footer' => esc_html__( 'Footer Menu', 'wpzaamx' ),
 	) );
 
 	/*
@@ -66,7 +80,15 @@ function wpzaamx_setup() {
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
 	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
+		'aside',             // title less blurb
+		'gallery',           // gallery of images
+		'link',              // quick link to other site
+		'image',             // an image
+		'quote',             // a quick quote
+		'status',            // a Facebook like status update
+		'video',             // video
+		'audio',             // audio
+		'chat'               // chat transcript
 	) );
 
 	// Set up the WordPress core custom background feature.
@@ -74,9 +96,15 @@ function wpzaamx_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
+
 }
 endif; // wpzaamx_setup
 add_action( 'after_setup_theme', 'wpzaamx_setup' );
+
+
+/* ==================================================================================================================================================== 
+ * ==================================================================================================================================================== 
+ */
 
 /**
  * Register widget area.
@@ -96,21 +124,88 @@ function wpzaamx_widgets_init() {
 }
 add_action( 'widgets_init', 'wpzaamx_widgets_init' );
 
+
+/* ==================================================================================================================================================== 
+ * ==================================================================================================================================================== 
+ */
+
 /**
  * Enqueue scripts and styles.
  */
 function wpzaamx_scripts() {
-	wp_enqueue_style( 'wpzaamx-style', get_stylesheet_uri() );
+	global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
 
-	wp_enqueue_script( 'wpzaamx-navigation', get_template_directory_uri() . '/library/javascripts/navigation.js', array(), '20120206', true );
+	/* ==================================================================================================================================================== 
+ 		/* register first !! styles and scripts
+		*/ 
+	if (!is_admin()) {
 
-	wp_enqueue_script( 'wpzaamx-skip-link-focus-fix', get_template_directory_uri() . '/library/javascripts/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+		// register fontAwesome
+		wp_register_style( 'wpzaamx-fontawesome', get_stylesheet_directory_uri() . '/library/fonts/font-awesome/css/font-awesome.min.css', array(), '', 'all' );
+
+		
+		// register main stylesheet
+		wp_register_style( 'wpzaamx-stylesheet', get_stylesheet_directory_uri() . '/library/stylesheets/styles.css', array(), '', 'all' );
+
+		// ie-only style sheet
+		wp_register_style( 'wpzaamx-ie-only', get_stylesheet_directory_uri() . '/library/stylesheets/ie.css', array(), '' );
+
+	    // comment reply script for threaded comments
+	    if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+			  wp_enqueue_script( 'comment-reply' );
+	    }
+
+		//adding scripts Bootstrap
+		wp_register_script( 'wpzaamx-bootstrapJS', get_stylesheet_directory_uri() . '/library/javascripts/bootstrap.min.js', array( 'jquery' ), '', true );
+
+		//adding scripts file in the footer
+		wp_register_script( 'wpzaamx-js', get_stylesheet_directory_uri() . '/library/javascripts/scripts.js', array( 'jquery' ), '', true );
+
+
+
+		/* ==================================================================================================================================================== 
+ 		/* enqueue styles and scripts
+		*/ 
+		
+		wp_enqueue_style( 'wpzaamx-fontawesome' );
+		wp_enqueue_style( 'wpzaamx-stylesheet' );
+		wp_enqueue_style( 'wpzaamx-ie-only' );
+
+		$wp_styles->add_data( 'wpzaamx-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
+
+		/*
+		I recommend using a plugin to call jQuery
+		using the google cdn. That way it stays cached
+		and your site will load faster.
+		*/
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'wpzaamx-bootstrapJS' );
+		wp_enqueue_script( 'wpzaamx-js' );
+
+		wp_enqueue_script( 'wpzaamx-navigation', get_template_directory_uri() . '/library/javascripts/navigation.js', array(), '20120206', true );
+		wp_enqueue_script( 'wpzaamx-skip-link-focus-fix', get_template_directory_uri() . '/library/javascripts/skip-link-focus-fix.js', array(), '20130115', true );
+
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wpzaamx_scripts' );
+
+/**
+ * Enqueue external fonts
+ */
+function wpzaamx_googlefonts() {
+  wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+  wp_enqueue_style( 'googleFonts');
+}
+
+add_action('wp_print_styles', 'wpzaamx_googlefonts');
+
+
+/* ==================================================================================================================================================== 
+ * ==================================================================================================================================================== 
+ */
+
+
 
 /**
  * Implement the Custom Header feature.
--- header.php
+++ header.php
@@ -10,15 +10,20 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php get_template_part( 'template-parts/header', 'extras' ); ?>
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+
+<div id="page" class="hfeed site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wpzaamx' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
